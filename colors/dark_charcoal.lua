1bb855f7a1bac1d3d13ecb2d40e7dc85c1d84b84
DepartamentodeSoluciones <mr.prieto00@gmail.com>
2026-08-29 22:04:52 -0600

    looking changes

--- colors/dark_charcoal.lua
+++ colors/dark_charcoal.lua
@@ -1,1 +1,7 @@
-require("colors.dark_charcoal").setup({ transparent = true })
+vim.cmd("hi clear")
+if vim.fn.exists("syntax_on") then
+	vim.cmd("syntax reset")
+end
+vim.g.colors_name = "dark_charcoal"
+
+require("dark_charcoal").setup({ transparent = true })
